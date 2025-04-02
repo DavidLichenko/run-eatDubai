f997c70ae97a1532815ad5ec81d86ba6515c07c0
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2025-04-02 15:04:52 +0200

    initial

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,60 @@
+import { MotionValue, motion, useSpring, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const fontSize = 30;
+const padding = 0;
+const height = fontSize + 10;
+
+export default function Counter({ value }: { value: number }) {
+    return (
+        <div
+            style={{ fontSize }}
+            className="flex space-x-3 overflow-hidden font-bold px-2 leading-none text-foreground"
+        >
+            {/*<Digit place={100} value={value} />*/}
+            <Digit place={10} value={value} />
+            <Digit place={1} value={value} />
+        </div>
+    );
+}
+
+function Digit({ place, value }: { place: number; value: number }) {
+    let valueRoundedToPlace = Math.floor(value / place);
+    let animatedValue = useSpring(valueRoundedToPlace);
+
+    useEffect(() => {
+        animatedValue.set(valueRoundedToPlace);
+    }, [animatedValue, valueRoundedToPlace]);
+
+    return (
+        <div style={{ height }} className="relative w-[1ch] tabular-nums">
+            {[...Array(10).keys()].map((i) => (
+                <Number key={i} mv={animatedValue} number={i} />
+            ))}
+        </div>
+    );
+}
+
+function Number({ mv, number }: { mv: MotionValue; number: number }) {
+    let y = useTransform(mv, (latest) => {
+        let placeValue = latest % 10;
+        let offset = (10 + number - placeValue) % 10;
+
+        let memo = offset * height;
+
+        if (offset > 5) {
+            memo -= 10 * height;
+        }
+
+        return memo;
+    });
+
+    return (
+        <motion.span
+            style={{ y }}
+            className="absolute inset-0 flex items-center justify-center"
+        >
+            {number}
+        </motion.span>
+    );
+}
