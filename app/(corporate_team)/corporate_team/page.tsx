7c70b9d535e326477e4dc24ee39f5fe068dd33b6
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2025-04-03 04:41:03 +0200

    initial

--- app/(corporate_team)/corporate_team/page.tsx
+++ app/(corporate_team)/corporate_team/page.tsx
@@ -9,9 +9,10 @@ const Page = () => {
         <>
             <Header/>
 
-            <div className="mx-auto h-screen container flex flex-col justify-center items-start">
-                <motion.div initial={{y:30, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 0.2, delay: 0.1, ease: "easeOut"}}>
-                <h2 className="text-2xl md:text-5xl font-bold mt-12 uppercase text-center ">CORPORATE TEAM</h2>
+            <div className="mx-auto h-screen container flex justify-center items-start">
+                <motion.div initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}}
+                            transition={{duration: 0.2, delay: 0.1, ease: "easeOut"}}>
+                    <h2 className="text-2xl md:text-5xl font-bold mt-12 uppercase text-center ">CORPORATE TEAM</h2>
                 </motion.div>
 
             </div>
