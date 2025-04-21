0f6e6f3146ea41800a73449d0c97f3a8be3726e6
DavidLichenko <89989857+DavidLichenko@users.noreply.github.com>
2025-04-21 16:23:50 +0200

    initial

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const FeatureCard = ({title,link}) => {
   return (
     <div className="flex gap-4 bg-secondary_text text-background text-center items-center justify-center py-4 px-8 rounded-sm shadow-md transition-colors duration-200 hover:cursor-pointer">
-      <Link href={link} className="text-xl text-center font-bold ">{title}</Link>
+      <a  href={link} target="_blank" className="text-xl text-center font-bold ">{title}</a>
     </div>
   );
 };
